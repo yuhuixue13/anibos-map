3ec01c86ca4243586e309760d931c6a6a9d6d8f5
yuhuixue13 <yuhuixue@bu.edu>
2025-11-25 17:38:15 -0500

    14

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,11 @@
 /* WRITE YOUR JS HERE... YOU MAY REQUIRE MORE THAN ONE JS FILE. IF SO SAVE IT SEPARATELY IN THE SCRIPTS DIRECTORY */
+/* const map = new Map('map', {
+    center: [51.505, -0.09],
+    zoom: 13
+}); */
+const map = new L.Map('map').setView([51.505, -0.09], 13);
+
+    const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
